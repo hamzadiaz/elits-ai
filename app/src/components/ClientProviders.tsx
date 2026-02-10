13fa1eac595b4e3119045f9dfc78428f483ccc57
hamzadiaz <hamza.diaz.house@gmail.com>
2026-02-10 21:42:05 +0100

    fix: WalletProvider context error - remove dynamic import, use direct client provider

--- app/src/components/ClientProviders.tsx
+++ app/src/components/ClientProviders.tsx
@@ -1,13 +1,8 @@
 'use client'
 
 import { ReactNode } from 'react'
-import dynamic from 'next/dynamic'
+import { WalletProviderWrapper } from '@/components/WalletProvider'
 import { ToastProvider } from '@/components/Toast'
-
-const WalletProviderWrapper = dynamic(
-  () => import('@/components/WalletProvider').then(m => m.WalletProviderWrapper),
-  { ssr: false }
-)
 
 export function ClientProviders({ children }: { children: ReactNode }) {
   return (
